1cf55f5634be5caf6ebd09fe65b47ddd8fb1f543
Ramazan Kalfa <gezgindeveloper1@gmail.com>
2023-02-16 19:08:36 +0300

    Django 'messages framework'yapisi ile Login olan kullanicinin bilgilendirmesi

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
+    if request.user.is_authenticated:
+        messages.info(request, f'{request.user.username} oy asiye asiye')
+        return redirect('home_view')
     context = dict()
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username , password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'{request.user.username} Login oldunuz')
+            return redirect('home_view')
     return render(request, 'user_profile/login.html', context)
