914f3b7e13e53861e236157c5d1870c1cb384f9f
Ramazan Kalfa <gezgindeveloper1@gmail.com>
2023-02-20 17:32:10 +0300

    'ModelForm' ile 'Blog Post Create' yapisinin olusturulmasi

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import create_blog_post_view
+
+
+app_name= 'blog'
+
+urlpatterns = [
+    path('create/', create_blog_post_view, name='create_blog_post_view'),
+
+]
--- config/urls.py
+++ config/urls.py
@@ -11,5 +11,6 @@ from page.views import home_view
 urlpatterns = [
     path('', home_view, name='home_view'),
     path('user/', include('user_profile.urls', namespace='user')),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
