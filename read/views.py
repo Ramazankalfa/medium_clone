bd856cbc9ebb59e928e3748960e631b4f8adcb27
Ramazan Kalfa <gezgindeveloper1@gmail.com>
2023-02-23 01:33:28 +0300

    'Get_absolute_url' kullanarak profil sayfasina yonlendirme

--- read/views.py
+++ read/views.py
@@ -3,8 +3,10 @@ from blog.models import Post
 from user_profile.models import Profile
 
 def all_posts_view(request, user_slug):
+    
     profile = get_object_or_404(Profile, slug=user_slug)
-    context=dict(
+    context = dict(
+        profile=profile,
         posts=Post.objects.filter(user=profile.user, is_active=True)
     )
     return render(request, 'read/all_post.html', context)
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from autoslug import AutoSlugField
@@ -9,3 +10,11 @@ class Profile(models.Model):
     instagram = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     
+
+    def get_absolute_url(self):
+        return reverse(
+            'read:all_posts_view',
+            kwargs={
+                "user_slug": self.slug,
+            }
+        )
