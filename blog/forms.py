376e26a61b1d4f7ea9069949c51fba4a74bb37ff
Ramazan Kalfa <gezgindeveloper1@gmail.com>
2023-02-22 17:31:54 +0300

    TagifyJS ile form-yapisina etiket sisteminin eklenmesi

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from .models import Post
 from tinymce.widgets import TinyMCE
+from django.core import validators
+
+
+def min_length_3(value):
+    if len(value) < 3:
+        raise forms.ValidationError("Opps. En az 3 karakter olmalıdır... ")
 
 class PostModelForm(forms.ModelForm):
     tag = forms.CharField()
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 20}))
+    title = forms.CharField(validators=[min_length_3, ])
 
 
     class Meta:
--- blog/views.py
+++ blog/views.py
@@ -6,14 +6,16 @@ from .models import Category, Tag, Post
 login_required(login_url='user:login_view')
 def create_blog_post_view(request):
     form=PostModelForm()
-    context = dict(
-        form = form
-    )
+   
     if request.method == 'POST':
         form=PostModelForm(request.POST or None, request.FILES or None)
         if form.is_valid():
             f=form.save(commit=False)
             f.user = request.user
-            f.save()
+            #f.save()
             print(form.cleaned_data)
+
+    context = dict(
+        form = form
+     )
     return render(request, 'blog/create_blog_post.html', context)
