809974423b78c83b6e269172313c3302ca746dbf
Ramazan Kalfa <gezgindeveloper1@gmail.com>
2023-02-24 18:45:08 +0300

    En cok okunan blog postlarinin listelenemesi

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def create_blog_post_view(request):
         form = form,    
         title = title,
      )
-    return render(request, 'blog/form.html', context)
+    return render(request, 'common_components/form.html', context)
 
 
 def category_view(request, category_slug):
--- page/views.py
+++ page/views.py
@@ -3,11 +3,13 @@ from blog.models import Post, Tag, Category
 
 def home_view(request):
     posts = Post.objects.filter(is_active=True)
+    top_posts = posts.order_by('-view_count')[:6]
     tags= Tag.objects.filter(is_active=True)
     categories = Category.objects.filter(is_active=True)
     context =dict(
         posts=posts,
         tags=tags,
         categories=categories,
+        top_posts=top_posts
     )
     return render(request, 'page/home_page.html', context)
